3665ff9ee8f3aee846f664f06ee682d8307f9e44
Ali <86948893+AleksHNZ@users.noreply.github.com>
2023-05-04 15:42:21 +0400

    <Testing react>

---
+++ react-test/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Greeting from './Greeting.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Greeting name = 'Ali'/>
+      </div>
+    </>
+  )
+}
+
+export default App
